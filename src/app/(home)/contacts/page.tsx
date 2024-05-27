75308b13ed69087630494d606d16a38199993c67
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-05-27 15:53:01 +0100

    contact us page

---
+++ src/app/(home)/contacts/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ContactUs } from "@/modules/constac";
+
+const page = () => {
+  return (
+    <>
+      <ContactUs />
+    </>
+  );
+};
+
+export default page;
