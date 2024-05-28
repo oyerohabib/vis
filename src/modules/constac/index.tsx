ea1f176d3f582a425bacc20e14e2cc0b6ca011a5
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-05-28 09:30:07 +0100

    open graph

--- src/modules/constac/index.tsx
+++ src/modules/constac/index.tsx
@@ -307,7 +307,7 @@ const ContactUs = () => {
         className="relative h-[400px] overflow-hidden  bg-cover bg-[50%] bg-no-repeat"
       >
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.6504987603735!2d3.4817529999999994!3d6.438911299999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103bf5f3726d455b%3A0xa2080292cedc2e88!2sViscio%20Express!5e0!3m2!1sen!2sng!4v1716823061961!5m2!1sen!2sng"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.6504987603735!2d3.4817529999999994!3d6.438911299999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103bf5f3726d455b%3A0xa2080292cedc2e88!2sViscio%20Express!5e0!3m2!1sen!2sng!4v1716884639057!5m2!1sen!2sng"
           width="100%"
           height="480"
           allowFullScreen
