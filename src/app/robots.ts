c9625284ae0fbfcf65b2be115dddad532ce82c16
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-05-28 11:19:35 +0100

    naub oages done

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: "*",
-        allow: "/",
+        allow: [`${baseUrl}`, `${baseUrl}/about-us`, "/contacts"],
         disallow: ["/admin:path", "/privacy"],
       },
     ],
